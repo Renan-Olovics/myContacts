1593e2ddcae2c99361f795212259eb32af3d7c71
Renan.Olovics <renan.olovics@gmail.com>
2022-05-16 22:51:21 -0300

    fix: import name

--- src/app/controllers/contactController.js
+++ src/app/controllers/contactController.js
@@ -1,16 +1,16 @@
-import { contactRepository } from '../repositories'
+import { contactsRepository } from '../repositories'
 
 class ContactController {
   async index (req, res) {
     const { orderBy } = req.query
-    const contacts = await contactRepository.findAll(orderBy)
+    const contacts = await contactsRepository.findAll(orderBy)
     res.json(contacts)
   }
 
   async show (req, res) {
     const { id } = req.params
 
-    const contact = await contactRepository.findById(id)
+    const contact = await contactsRepository.findById(id)
     if (!contact) return res.status(404).json({ error: 'Contact not found' })
 
     res.json(contact)
@@ -21,10 +21,10 @@ class ContactController {
 
     if (!name) return res.status(400).json({ error: 'Name is required' })
 
-    const contactExists = await contactRepository.findByEmail(email)
+    const contactExists = await contactsRepository.findByEmail(email)
     if (contactExists) return res.status(400).json({ error: 'This e-mail is already in use' })
 
-    const contact = await contactRepository.create({ name, email, phone, category_id })
+    const contact = await contactsRepository.create({ name, email, phone, category_id })
 
     return res.json(contact)
   }
@@ -33,23 +33,23 @@ class ContactController {
     const { id } = req.params
     const { name, email, phone, category_id } = req.body
 
-    const contactExists = await contactRepository.findById(id)
+    const contactExists = await contactsRepository.findById(id)
     if (!contactExists) return res.status(404).json({ error: 'Contact not found' })
     if (!name) return res.status(400).json({ error: 'Name is required' })
 
-    const contactByEmail = await contactRepository.findByEmail(email)
+    const contactByEmail = await contactsRepository.findByEmail(email)
     if (contactByEmail && contactByEmail.id !== id) {
       return res.status(400).json({ error: 'This e-mail is already in use' })
     }
 
-    const contact = await contactRepository.update(id, { name, email, phone, category_id })
+    const contact = await contactsRepository.update(id, { name, email, phone, category_id })
 
     res.json(contact)
   }
 
   async delete (req, res) {
     const { id } = req.params
-    await contactRepository.delete(id)
+    await contactsRepository.delete(id)
     res.sendStatus(204)
   }
 }
